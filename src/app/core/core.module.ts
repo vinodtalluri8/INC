2172775024dce9e9279539845540c6de649bbd80
Talluri, Vinod-Kumar (Boston) <VTalluri@MFS.com>
2018-06-22 10:47:19 -0400

    check npm

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+/**
+ * This file should contains imported and exported
+ * components for code module
+ */
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AppRoutingModule } from '../app-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AngularFontAwesomeModule,
+    AppRoutingModule
+  ],
+  declarations: [NavbarComponent],
+  exports: [NavbarComponent]
+})
+export class CoreModule { }
