53683a9b578f779bd3510528afd63ca26ddb47d9
Krishna <msarath590@gmail.com>
2018-07-12 03:10:10 +0530

    maintancr added

---
+++ src/app/controls/maintenance/new-control-objective/new-control-objective.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-control-objective',
+  templateUrl: './new-control-objective.component.html',
+  styleUrls: ['./new-control-objective.component.css']
+})
+export class NewControlObjectiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/controls/maintenance/update-control-objective/update-control-objective.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-control-objective',
+  templateUrl: './update-control-objective.component.html',
+  styleUrls: ['./update-control-objective.component.css']
+})
+export class UpdateControlObjectiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
